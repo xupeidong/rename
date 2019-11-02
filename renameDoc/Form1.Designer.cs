bf71405b8fdeadb60860d8766600626dce99c53f
xupeidong <xupeidong@ainfinit.com>
2019-11-02 11:27:19 +0800

    modify

---
+++ renameDoc/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace renameDoc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_select = new System.Windows.Forms.Button();
+            this.textBox_docDir = new System.Windows.Forms.TextBox();
+            this.richTextBox_docList = new System.Windows.Forms.RichTextBox();
+            this.textBox_fresh = new System.Windows.Forms.TextBox();
+            this.textBox_rename = new System.Windows.Forms.TextBox();
+            this.label_fresh = new System.Windows.Forms.Label();
+            this.label_rename = new System.Windows.Forms.Label();
+            this.richTextBox_rename = new System.Windows.Forms.RichTextBox();
+            this.button_begin = new System.Windows.Forms.Button();
+            this.button_end = new System.Windows.Forms.Button();
+            this.label_select = new System.Windows.Forms.Label();
+            this.textBox_select = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_select
+            // 
+            this.button_select.Location = new System.Drawing.Point(12, 22);
+            this.button_select.Name = "button_select";
+            this.button_select.Size = new System.Drawing.Size(75, 23);
+            this.button_select.TabIndex = 0;
+            this.button_select.Text = "选择";
+            this.button_select.UseVisualStyleBackColor = true;
+            this.button_select.Click += new System.EventHandler(this.button_select_Click);
+            // 
+            // textBox_docDir
+            // 
+            this.textBox_docDir.Location = new System.Drawing.Point(103, 22);
+            this.textBox_docDir.Name = "textBox_docDir";
+            this.textBox_docDir.Size = new System.Drawing.Size(571, 21);
+            this.textBox_docDir.TabIndex = 1;
+            // 
+            // richTextBox_docList
+            // 
+            this.richTextBox_docList.Location = new System.Drawing.Point(12, 65);
+            this.richTextBox_docList.Name = "richTextBox_docList";
+            this.richTextBox_docList.Size = new System.Drawing.Size(662, 542);
+            this.richTextBox_docList.TabIndex = 2;
+            this.richTextBox_docList.Text = "";
+            // 
+            // textBox_fresh
+            // 
+            this.textBox_fresh.Location = new System.Drawing.Point(804, 24);
+            this.textBox_fresh.Name = "textBox_fresh";
+            this.textBox_fresh.Size = new System.Drawing.Size(284, 21);
+            this.textBox_fresh.TabIndex = 5;
+            // 
+            // textBox_rename
+            // 
+            this.textBox_rename.Location = new System.Drawing.Point(804, 65);
+            this.textBox_rename.Name = "textBox_rename";
+            this.textBox_rename.Size = new System.Drawing.Size(284, 21);
+            this.textBox_rename.TabIndex = 6;
+            // 
+            // label_fresh
+            // 
+            this.label_fresh.AutoSize = true;
+            this.label_fresh.Location = new System.Drawing.Point(709, 31);
+            this.label_fresh.Name = "label_fresh";
+            this.label_fresh.Size = new System.Drawing.Size(89, 12);
+            this.label_fresh.TabIndex = 7;
+            this.label_fresh.Text = "刷新频率(秒)：";
+            // 
+            // label_rename
+            // 
+            this.label_rename.AutoSize = true;
+            this.label_rename.Location = new System.Drawing.Point(711, 74);
+            this.label_rename.Name = "label_rename";
+            this.label_rename.Size = new System.Drawing.Size(77, 12);
+            this.label_rename.TabIndex = 8;
+            this.label_rename.Text = "重命名字符：";
+            // 
+            // richTextBox_rename
+            // 
+            this.richTextBox_rename.Location = new System.Drawing.Point(711, 262);
+            this.richTextBox_rename.Name = "richTextBox_rename";
+            this.richTextBox_rename.Size = new System.Drawing.Size(377, 345);
+            this.richTextBox_rename.TabIndex = 9;
+            this.richTextBox_rename.Text = "";
+            // 
+            // button_begin
+            // 
+            this.button_begin.Location = new System.Drawing.Point(711, 185);
+            this.button_begin.Name = "button_begin";
+            this.button_begin.Size = new System.Drawing.Size(85, 37);
+            this.button_begin.TabIndex = 10;
+            this.button_begin.Text = "开始";
+            this.button_begin.UseVisualStyleBackColor = true;
+            this.button_begin.Click += new System.EventHandler(this.button_begin_Click);
+            // 
+            // button_end
+            // 
+            this.button_end.Location = new System.Drawing.Point(1003, 185);
+            this.button_end.Name = "button_end";
+            this.button_end.Size = new System.Drawing.Size(85, 37);
+            this.button_end.TabIndex = 11;
+            this.button_end.Text = "结束";
+            this.button_end.UseVisualStyleBackColor = true;
+            this.button_end.Click += new System.EventHandler(this.button_end_Click);
+            // 
+            // label_select
+            // 
+            this.label_select.AutoSize = true;
+            this.label_select.Location = new System.Drawing.Point(713, 120);
+            this.label_select.Name = "label_select";
+            this.label_select.Size = new System.Drawing.Size(71, 12);
+            this.label_select.TabIndex = 12;
+            this.label_select.Text = "筛     选：";
+            this.label_select.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox_select
+            // 
+            this.textBox_select.Location = new System.Drawing.Point(804, 110);
+            this.textBox_select.Name = "textBox_select";
+            this.textBox_select.Size = new System.Drawing.Size(284, 21);
+            this.textBox_select.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 634);
+            this.Controls.Add(this.textBox_select);
+            this.Controls.Add(this.label_select);
+            this.Controls.Add(this.button_end);
+            this.Controls.Add(this.button_begin);
+            this.Controls.Add(this.richTextBox_rename);
+            this.Controls.Add(this.label_rename);
+            this.Controls.Add(this.label_fresh);
+            this.Controls.Add(this.textBox_rename);
+            this.Controls.Add(this.textBox_fresh);
+            this.Controls.Add(this.richTextBox_docList);
+            this.Controls.Add(this.textBox_docDir);
+            this.Controls.Add(this.button_select);
+            this.Name = "Form1";
+            this.Text = "暴躁的黄金五老牛";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_select;
+        private System.Windows.Forms.TextBox textBox_docDir;
+        private System.Windows.Forms.RichTextBox richTextBox_docList;
+        private System.Windows.Forms.TextBox textBox_fresh;
+        private System.Windows.Forms.TextBox textBox_rename;
+        private System.Windows.Forms.Label label_fresh;
+        private System.Windows.Forms.Label label_rename;
+        private System.Windows.Forms.RichTextBox richTextBox_rename;
+        private System.Windows.Forms.Button button_begin;
+        private System.Windows.Forms.Button button_end;
+        private System.Windows.Forms.Label label_select;
+        private System.Windows.Forms.TextBox textBox_select;
+    }
+}
+
---
+++ renameDoc/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace renameDoc
+{
+    public partial class Form1 : Form
+    {
+        Timer pTimer = new Timer();
+        List<string> pList = new List<string>();
+        private static string configpath = Application.StartupPath + "\\config.ini";
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filepath);
+
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retval, int size, string filePath);
+
+        public static void WriteContentValue(string section, string key, string iValue)
+        {
+            WritePrivateProfileString(section, key, iValue, configpath);
+        }
+
+        public static string ReadContentValue(string Section, string key)
+        {
+            StringBuilder temp = new StringBuilder(1024);
+            GetPrivateProfileString(Section, key, "", temp, 1024, configpath);
+            return temp.ToString();
+        }
+     public static T ReadConfig<T>(string section, string key)
+         {
+             if (File.Exists(configpath))
+             {
+                string value = ReadContentValue(section, key);
+
+                if (String.IsNullOrWhiteSpace(value))
+                     return default(T);
+ 
+                if (typeof(T).IsEnum)
+                     return (T) Enum.Parse(typeof(T), value, true);
+ 
+                 return (T) Convert.ChangeType(value, typeof(T));
+             }
+             else
+             {
+                 return default(T);
+            }
+        }
+
+       public static void WriteConfig(string section, string key, string value)
+        {
+             //如果文件不存在，则创建
+             if (!File.Exists(configpath))
+             {
+                 using (FileStream myFs = new FileStream(configpath, FileMode.Create)) { }
+             }
+             WriteContentValue(section, key, value);
+         }
+
+public static bool connectState(string path)
+        {
+            return connectState(path, "", "");
+        }
+        /// <summary>
+        /// 连接远程共享文件夹
+        /// </summary>
+        /// <param name="path">远程共享文件夹的路径</param>
+        /// <param name="userName">用户名</param>
+        /// <param name="passWord">密码</param>
+        /// <returns></returns>
+        public static bool connectState(string path, string userName, string passWord)
+        {
+            bool Flag = false;
+            Process proc = new Process();
+            try
+            {
+                proc.StartInfo.FileName = "cmd.exe";
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardInput = true;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.Start();
+                string dosLine = "net use " + path + " " + passWord + " /user:" + userName;
+                proc.StandardInput.WriteLine(dosLine);
+                proc.StandardInput.WriteLine("exit");
+                while (!proc.HasExited)
+                {
+                    proc.WaitForExit(1000);
+                }
+                string errormsg = proc.StandardError.ReadToEnd();
+                proc.StandardError.Close();
+                if (string.IsNullOrEmpty(errormsg))
+                {
+                    Flag = true;
+                }
+                else
+                {
+                    throw new Exception(errormsg);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Data);
+                //throw ex;
+            }
+            finally
+            {
+                proc.Close();
+                proc.Dispose();
+            }
+            return Flag;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.button_end.Enabled = false;
+            pTimer.Tick += new EventHandler(callback);
+            pTimer.Enabled = false;
+
+            this.textBox_docDir.Text = ReadConfig<string>("doc", "path");
+            this.textBox_fresh.Text = ReadConfig<string>("doc", "time");
+            this.textBox_rename.Text = ReadConfig<string>("doc", "name");
+        }
+
+        private void callback(object sender, EventArgs e)
+        {
+            this.richTextBox_docList.Clear();
+            string strPath = this.textBox_docDir.Text;
+            DirectoryInfo root = new DirectoryInfo(strPath);
+            FileInfo[] files = root.GetFiles();
+            this.richTextBox_docList.AppendText(Convert.ToString(files.Length));
+            this.richTextBox_docList.AppendText("\n");
+            for (int i = 0; i < files.Length; i++)
+            {
+                this.richTextBox_docList.AppendText(files.GetValue(i).ToString());
+                this.richTextBox_docList.AppendText("\n");
+            }
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                string tmpFileName = files.GetValue(i).ToString();
+                bool isHave = false;
+                for(int j = 0; j < pList.Count; j++)
+                {
+                    if(pList[j].Contains(tmpFileName))
+                    {
+                        isHave = true;
+                        break;
+                    }
+                }
+                if(isHave)
+                {
+                    continue;
+                }
+                string tmpNewFileName = files.GetValue(i).ToString();
+                string tmpStrSelect = this.textBox_select.Text;
+                if(tmpStrSelect != "")
+                {
+                    if (tmpNewFileName.Contains(tmpStrSelect) && !tmpNewFileName.Contains("【") && !tmpNewFileName.Contains("】"))
+                    {
+                        pList.Add(tmpNewFileName);
+                        string oldpath = this.textBox_docDir.Text + @"\" + tmpNewFileName;
+                        string newpath = this.textBox_docDir.Text + @"\" + tmpNewFileName.Insert(2, this.textBox_rename.Text);
+                        Console.WriteLine(oldpath);
+                        Console.WriteLine(newpath);
+                        FileInfo fileInfo = new FileInfo(oldpath);
+                        fileInfo.MoveTo(newpath);
+                        this.richTextBox_rename.AppendText(tmpNewFileName.Insert(2, this.textBox_rename.Text));
+                        pList.Add(tmpNewFileName.Insert(2, this.textBox_rename.Text));
+                        this.richTextBox_rename.AppendText("\n");
+                        break;
+                    }
+                }else
+                {
+                    if (!tmpNewFileName.Contains("【") && !tmpNewFileName.Contains("】"))
+                    {
+                        pList.Add(tmpNewFileName);
+                        string oldpath = this.textBox_docDir.Text + @"\" + tmpNewFileName;
+                        string newpath = this.textBox_docDir.Text + @"\" + tmpNewFileName.Insert(2, this.textBox_rename.Text);
+                        Console.WriteLine(oldpath);
+                        Console.WriteLine(newpath);
+                        FileInfo fileInfo = new FileInfo(oldpath);
+                        fileInfo.MoveTo(newpath);
+                        this.richTextBox_rename.AppendText(tmpNewFileName.Insert(2, this.textBox_rename.Text));
+                        pList.Add(tmpNewFileName.Insert(2, this.textBox_rename.Text));
+                        this.richTextBox_rename.AppendText("\n");
+                        break;
+                    }
+                }
+            }
+
+            for(int i = 0; i < files.Length; i++)
+            {
+                string tmpFileName = files.GetValue(i).ToString();
+                bool isHave = false;
+                for (int j = 0; j < pList.Count; j++)
+                {
+                    if (pList[j].Contains(tmpFileName))
+                    {
+                        isHave = true;
+                        break;
+                    }
+                }
+                if (isHave)
+                {
+                    continue;
+                }else
+                {
+                    pList.Add(tmpFileName);
+                }
+            }
+
+        }
+
+        private void button_select_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            path.ShowDialog();
+            string strPath = path.SelectedPath;
+            this.textBox_docDir.Text = strPath;
+            if(strPath == "")
+            {
+                return;
+            }
+
+            bool bresult = connectState(strPath);
+
+            DirectoryInfo root = new DirectoryInfo(strPath);
+            FileInfo[] files = root.GetFiles();
+            for(int i = 0; i < files.Length; i++)
+            {
+                this.richTextBox_docList.AppendText(files.GetValue(i).ToString());
+                this.richTextBox_docList.AppendText("\n");
+            }
+        }
+
+        private void button_begin_Click(object sender, EventArgs e)
+        {
+            if(this.textBox_fresh.Text == "")
+            {
+                MessageBox.Show("刷新频率不能为空", "警告");
+                return;
+            }
+            if(this.textBox_rename.Text == "")
+            {
+                MessageBox.Show("重命名字符不能为空", "警告");
+                return;
+            }
+
+            WriteConfig("doc", "path", this.textBox_docDir.Text);
+            WriteConfig("doc", "time", this.textBox_fresh.Text);
+            WriteConfig("doc", "name", this.textBox_rename.Text);
+
+            pList.Clear();
+
+            string strPath = this.textBox_docDir.Text;
+            DirectoryInfo root = new DirectoryInfo(strPath);
+            FileInfo[] files = root.GetFiles();
+            for (int i = 0; i < files.Length; i++)
+            {
+                pList.Add(files.GetValue(i).ToString());
+            }
+
+            this.button_begin.Enabled = false;
+            this.button_end.Enabled = true;
+
+            pTimer.Enabled = true;
+            pTimer.Interval = Convert.ToInt32(this.textBox_fresh.Text) * 1000;
+            pTimer.Start();
+        }
+
+        private void button_end_Click(object sender, EventArgs e)
+        {
+            this.button_begin.Enabled = true;
+            this.button_end.Enabled = false;
+            pTimer.Enabled = false;
+            pTimer.Stop();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
